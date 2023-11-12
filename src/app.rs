e72ac847349f920763a181a36d014c28138f9a54
Cameron Dubas <camerondubas@gmail.com>
2023-11-12 13:50:44 +0000

    fix: enable pointer events on message

--- src/app.rs
+++ src/app.rs
@@ -18,7 +18,7 @@ pub fn App(
         <Html class="pointer-events-none" />
 
         <div class="static">
-            <div class="absolute bottom-0 right-0 bg-white dark:bg-slate-800 rounded-xl shadow-lg m-4">
+            <div class="pointer-events-auto absolute bottom-0 right-0 bg-white dark:bg-slate-800 rounded-xl shadow-lg m-4">
                 <p class="p-4 text-slate-500 dark:text-slate-400">"Hello, world! Message: "{name.clone()}</p>
             </div>
             <ControlPanel events={events.clone()} />
