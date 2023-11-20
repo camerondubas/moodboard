e624045f5cdcfd067d0815dcd18c8358e244488a
Cameron Dubas <camerondubas@gmail.com>
2023-11-20 21:53:51 +0000

    feat(theme):  initial pass at theming components

--- game/src/post_it.rs
+++ game/src/post_it.rs
@@ -6,9 +6,10 @@ use crate::{
     item::Item,
     prelude::*,
     select::components::{Selectable, Selected},
+    theme::{ThemeOptions, ThemeStyle},
 };
 
-const POST_IT_COLORS: [Color; 6] = [
+const POST_IT_LIGHT_COLORS: [Color; 6] = [
     Palette::AMBER_200,
     Palette::GREEN_300,
     Palette::PURPLE_300,
@@ -17,12 +18,23 @@ const POST_IT_COLORS: [Color; 6] = [
     Palette::SLATE_400,
 ];
 
+const POST_IT_DARK_COLORS: [Color; 6] = [
+    Palette::AMBER_700,
+    Palette::GREEN_600,
+    Palette::PURPLE_600,
+    Palette::BLUE_600,
+    Palette::PINK_700,
+    Palette::SLATE_600,
+];
+
 const POST_IT_SIZE: Vec2 = Vec2::new(400., 420.);
 const POST_IT_STROKE_WIDTH: f32 = 5.0;
 const POST_IT_STROKE_ALPHA: f32 = 0.7;
 const POST_IT_STROKE_COLOR: Color = Color::BLACK;
-const POST_IT_SHADOW_COLOR: Color = Palette::GRAY_600;
-const POST_IT_SHADOW_ALPHA: f32 = 0.6;
+const POST_IT_SHADOW_COLOR_LIGHT: Color = Palette::GRAY_600;
+const POST_IT_SHADOW_COLOR_DARK: Color = Palette::GRAY_600;
+const POST_IT_SHADOW_ALPHA_LIGHT: f32 = 0.6;
+const POST_IT_SHADOW_ALPHA_DARK: f32 = 0.9;
 const POST_IT_STROKE_WIDTH_SELECTED: f32 = 10.0;
 
 pub struct PostItPlugin;
@@ -49,52 +61,61 @@ fn draw_initial_post_its(mut commands: Commands) {
     draw_post_it(
         &mut commands,
         Vec3::new(-600., 300., 0.0),
-        Palette::AMBER_200,
+        POST_IT_LIGHT_COLORS[0],
+        POST_IT_DARK_COLORS[0],
         text,
     );
 
     draw_post_it(
         &mut commands,
         Vec3::new(0., 300., 0.0),
-        Palette::GREEN_300,
+        POST_IT_LIGHT_COLORS[1],
+        POST_IT_DARK_COLORS[1],
         text,
     );
 
     draw_post_it(
         &mut commands,
         Vec3::new(600., 300., 0.0),
-        Palette::PURPLE_300,
+        POST_IT_LIGHT_COLORS[2],
+        POST_IT_DARK_COLORS[2],
         text,
     );
 
     draw_post_it(
         &mut commands,
         Vec3::new(-600., -300., 0.0),
-        Palette::BLUE_300,
+        POST_IT_LIGHT_COLORS[3],
+        POST_IT_DARK_COLORS[3],
         text,
     );
 
     draw_post_it(
         &mut commands,
         Vec3::new(0., -300., 0.0),
-        Palette::PINK_300,
+        POST_IT_LIGHT_COLORS[4],
+        POST_IT_DARK_COLORS[4],
         text,
     );
 
     draw_post_it(
         &mut commands,
         Vec3::new(600., -300., 0.0),
-        Palette::SLATE_400,
+        POST_IT_LIGHT_COLORS[5],
+        POST_IT_DARK_COLORS[5],
         text,
     );
 }
 
 fn add_post_it(mut commands: Commands, mut events: EventReader<AddPostItEvent>) {
     for event in events.read() {
-        let color = POST_IT_COLORS.choose(&mut rand::thread_rng()).unwrap();
+        let color = POST_IT_LIGHT_COLORS
+            .choose(&mut rand::thread_rng())
+            .unwrap();
         draw_post_it(
             &mut commands,
             Vec3::new(0., 0., 0.0),
+            *color,
             *color,
             event.0.as_str(),
         );
@@ -120,10 +141,22 @@ fn remove_select(
     }
 }
 
-fn draw_post_it(commands: &mut Commands, position: Vec3, color: Color, text: &str) {
-    let text_style = TextStyle {
+fn draw_post_it(
+    commands: &mut Commands,
+    position: Vec3,
+    light_color: Color,
+    dark_color: Color,
+    text: &str,
+) {
+    let text_style_light = TextStyle {
         font_size: 32.0,
         color: Palette::GRAY_700.with_a(0.8),
+        ..Default::default()
+    };
+
+    let text_style_dark = TextStyle {
+        font_size: 32.0,
+        color: Palette::GRAY_200.with_a(0.8),
         ..Default::default()
     };
 
@@ -137,11 +170,20 @@ fn draw_post_it(commands: &mut Commands, position: Vec3, color: Color, text: &st
                 spatial: SpatialBundle::from_transform(Transform::from_translation(position)),
                 ..Default::default()
             },
-            Fill::color(color),
             Stroke::new(
                 POST_IT_STROKE_COLOR.with_a(POST_IT_STROKE_ALPHA),
                 POST_IT_STROKE_WIDTH,
             ),
+            ThemeStyle {
+                dark: ThemeOptions {
+                    fill: Some(Fill::color(dark_color)),
+                    ..Default::default()
+                },
+                light: ThemeOptions {
+                    fill: Some(Fill::color(light_color)),
+                    ..Default::default()
+                },
+            },
             Selectable,
             Item,
             PostIt,
@@ -159,24 +201,52 @@ fn draw_post_it(commands: &mut Commands, position: Vec3, color: Color, text: &st
                     ))),
                     ..Default::default()
                 },
-                Fill::color(POST_IT_SHADOW_COLOR.with_a(POST_IT_SHADOW_ALPHA)),
+                ThemeStyle {
+                    dark: ThemeOptions {
+                        fill: Some(Fill::color(
+                            POST_IT_SHADOW_COLOR_DARK.with_a(POST_IT_SHADOW_ALPHA_DARK),
+                        )),
+                        ..Default::default()
+                    },
+                    light: ThemeOptions {
+                        fill: Some(Fill::color(
+                            POST_IT_SHADOW_COLOR_LIGHT.with_a(POST_IT_SHADOW_ALPHA_LIGHT),
+                        )),
+                        ..Default::default()
+                    },
+                },
                 Name::new("Post-it Note Shadow"),
             ));
 
-            builder.spawn(Text2dBundle {
-                text: Text {
-                    sections: vec![TextSection::new(text, text_style.clone())],
-                    alignment: TextAlignment::Left,
-                    linebreak_behavior: BreakLineOn::WordBoundary,
-                },
-                text_2d_bounds: Text2dBounds {
-                    // Wrap text in the rectangle
-                    size: POST_IT_SIZE * 0.8,
-                },
+            builder.spawn((
+                Text2dBundle {
+                    text_2d_bounds: Text2dBounds {
+                        // Wrap text in the rectangle
+                        size: POST_IT_SIZE * 0.8,
+                    },
 
-                // ensure the text is drawn on top of the box
-                transform: Transform::from_translation(Vec3::new(0., 0., 0.1)),
-                ..default()
-            });
+                    // ensure the text is drawn on top of the box
+                    transform: Transform::from_translation(Vec3::new(0., 0., 0.1)),
+                    ..default()
+                },
+                ThemeStyle {
+                    dark: ThemeOptions {
+                        text: Some(Text {
+                            sections: vec![TextSection::new(text, text_style_dark.clone())],
+                            alignment: TextAlignment::Left,
+                            linebreak_behavior: BreakLineOn::WordBoundary,
+                        }),
+                        ..Default::default()
+                    },
+                    light: ThemeOptions {
+                        text: Some(Text {
+                            sections: vec![TextSection::new(text, text_style_light.clone())],
+                            alignment: TextAlignment::Left,
+                            linebreak_behavior: BreakLineOn::WordBoundary,
+                        }),
+                        ..Default::default()
+                    },
+                },
+            ));
         });
 }
--- game/src/theme/mod.rs
+++ game/src/theme/mod.rs
@@ -1,4 +1,6 @@
 pub mod colors;
+use bevy::window::WindowThemeChanged;
+
 use crate::events::ThemeEvent;
 use crate::prelude::*;
 
@@ -12,11 +14,38 @@ pub struct ThemePlugin;
 
 impl Plugin for ThemePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, on_theme_change);
+        app.add_systems(Update, (on_theme_change, on_theme_change_system));
     }
 }
 
-fn on_theme_change(mut commands: Commands, mut theme_event_reader: EventReader<ThemeEvent>) {
+#[derive(Component, Default)]
+pub(crate) struct ThemeStyle {
+    pub dark: ThemeOptions,
+    pub light: ThemeOptions,
+}
+
+impl ThemeStyle {
+    #[allow(dead_code)]
+    pub fn new(dark: ThemeOptions, light: ThemeOptions) -> Self {
+        Self { dark, light }
+    }
+}
+
+#[derive(Default)]
+pub struct ThemeOptions {
+    pub fill: Option<Fill>,
+    pub stroke: Option<Stroke>,
+    pub text: Option<Text>,
+}
+
+fn on_theme_change(
+    mut commands: Commands,
+    mut theme_event_reader: EventReader<ThemeEvent>,
+    theme_style_query: Query<(Entity, &ThemeStyle)>,
+    mut fill_query: Query<&mut Fill>,
+    mut stroke_query: Query<&mut Stroke>,
+    mut text_query: Query<&mut Text>,
+) {
     for event in theme_event_reader.read() {
         let bg_color = match event.theme {
             Theme::Dark => Palette::SLATE_900,
@@ -24,5 +53,51 @@ fn on_theme_change(mut commands: Commands, mut theme_event_reader: EventReader<T
         };
 
         commands.insert_resource(ClearColor(bg_color));
+
+        for (entity, theme_style) in &theme_style_query {
+            let target_theme = match event.theme {
+                Theme::Light => &theme_style.light,
+                Theme::Dark => &theme_style.dark,
+            };
+
+            if let Some(target_fill) = target_theme.fill {
+                match fill_query.get_component_mut::<Fill>(entity) {
+                    Ok(mut fill) => {
+                        *fill = target_fill;
+                    }
+                    Err(_) => {
+                        commands.entity(entity).insert(target_fill);
+                    }
+                }
+            }
+
+            if let Some(target_stroke) = target_theme.stroke {
+                match stroke_query.get_component_mut::<Stroke>(entity) {
+                    Ok(mut stroke) => {
+                        *stroke = target_stroke;
+                    }
+                    Err(_) => {
+                        commands.entity(entity).insert(target_stroke);
+                    }
+                }
+            }
+
+            if let Some(target_text) = &target_theme.text {
+                match text_query.get_component_mut::<Text>(entity) {
+                    Ok(mut text) => {
+                        *text = target_text.clone();
+                    }
+                    Err(_) => {
+                        commands.entity(entity).insert(target_text.clone());
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn on_theme_change_system(mut theme_event_reader: EventReader<WindowThemeChanged>) {
+    for event in theme_event_reader.read() {
+        info!("Theme changed: {:?}", event.theme);
     }
 }
